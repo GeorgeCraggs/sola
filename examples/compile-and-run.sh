2c735291e31cf3ddf4a98222076ea353d7760de5
George Craggs <george@gac.sh>
2021-12-31 19:28:05 +0000

    Fix Counter and Todo examples

--- examples/compile-and-run.sh
+++ examples/compile-and-run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #make
-#deno run --allow-read --allow-write=build/ ../compile.ts components/*.sola.html
-deno run --allow-read --allow-write=build/ ../compile.ts components/AdvancedTodo.sola.html
+deno run --allow-read --allow-write=build/ ../compile.ts components/*.sola.html
+#deno run --allow-read --allow-write=build/ ../compile.ts components/AdvancedTodo.sola.html
 
 deno run --allow-net server.ts
